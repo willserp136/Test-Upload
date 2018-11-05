1469cadc7359b1225fb3da3179be60775ad5ab51
Will <serpaw@wit.edu>
2018-11-05 09:17:14 -0500

    first upload

---
+++ Lab1_Problem1/src/lab1p1.java
@@ -0,0 +1,5 @@
+public class lab1p1 {
+public static void main(String[] args) {
+System.out.println("I can't wait to learn more about java, I don't want to be stuck doing this low level stuff");
+}
+}
